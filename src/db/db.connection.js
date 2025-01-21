6af2f88d63e42a4c52785eb9ecada9c809236e57
Pratik Sarvaiya <sarvaiyapratik09@gmail.com>
2025-01-21 23:44:32 +0530

    add vercel.json for deployment

--- src/db/db.connection.js
+++ src/db/db.connection.js
@@ -3,8 +3,6 @@ import {DB_NAME} from "../constants.js"
 
 const connectDB = async () => {
     try {
-
-        // console.log(process.env.MONGODB_URI)
         
         const response = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`MongoDB Connected !!`)
